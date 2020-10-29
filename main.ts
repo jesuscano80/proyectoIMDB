8ba7fb0f326935c7f8510400bc9b3741efc342fc
jesuscano80 <jesuscano@yahoo.es>
2020-10-29 16:02:22 +0100

    subimos main

---
+++ main.ts
@@ -0,0 +1,5 @@
+import {Professional,Genero} from "./professional"
+
+let nuevoprofesional= new Professional("jose",28,Genero.masculino,80,180,"rubio","azules","amplia",true,"española",4,"director");
+let nuevoprofesional1= new Professional ("juan",20,Genero.masculino,82,190,"moreno","verdes","corta",false,"inglesa",1,"actor");
+console.log(nuevoprofesional.toString());
